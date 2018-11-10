92394d04d25054741c7338f54c0070ec839ef12f
Thắng <bjnngokm@gmail.com>
2018-11-10 12:12:48 +0700

    Online Help Desk

---
+++ Thang/Online Help Desk/Online Help Desk/Models/DbModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Online_Help_Desk.Models;
+
+namespace Online_Help_Desk.Models
+{
+    public class DbModel : DbContext
+    { 
+        public DbModel() : base("OHDCon") { }
+        
+        public DbSet<Admin> Admin { get; set; }
+        public DbSet<Staff> StaffList { get; set; }
+        public DbSet<Facility> FacilityList { get; set; }
+        public DbSet<EndUser> UserList { get; set; }
+        public DbSet<Request> RequestList { get; set; }
+        public DbSet<Reply> ReplyList { get; set; }
+        public DbSet<Register> RegisterList { get; set; }
+    }
+}
---
+++ Thang/Online Help Desk/Online Help Desk/Models/Facility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Online_Help_Desk.Models
+{
+    public class Facility
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string FacilityID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FClass { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FType { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+        public DateTime FDate { get; set; }
+
+    }
+}
