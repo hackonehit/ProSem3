e87f9d9e9bcfaa9808e5d9fadaba1383f2d944fa
Thắng <bjnngokm@gmail.com>
2018-11-11 00:10:55 +0700

    Up Staff, User trong folder Thang2

---
+++ Thang2/Online Help Desk/Online Help Desk/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Online_Help_Desk.Models;
+
+namespace Online_Help_Desk.Controllers
+{
+    public class HomeController : Controller
+    {
+        DbModel db = new DbModel();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Admin);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Admin newAdmin)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry<Admin>(newAdmin).State = System.Data.Entity.EntityState.Added;
+                    db.SaveChanges();
+                    ModelState.AddModelError("", "Add complete!!!");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Fail");
+                }
+            }
+            catch (Exception e)
+            {
+
+                ModelState.AddModelError("", e.Message);
+            }
+            return View();
+        }
+        
+    }
+}
