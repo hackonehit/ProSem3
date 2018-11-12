9de57747553f12e29aa420c1d03362089a375be6
namchamvinhcuu <39082021+namchamvinhcuu@users.noreply.github.com>
2018-11-12 16:29:37 +0700

    update solution

---
+++ Thang/Online Help Desk/Online Help Desk/Models/Request.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Online_Help_Desk.Models
+{
+    public class Request
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+        public int RequestID { get; set; }
+        
+        
+        public string FacilityID { get; set; }
+        [Required]
+        public string EUUsername { get; set; }
+        public string SUsername { get; set; }
+        [Required]
+        [Range(0,8)]
+        public int RStatus { get; set; }
+        [Required]
+        
+        public DateTime DateTime { get; set; }
+        public string RImage { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(200)]
+        
+        public string Remarks{ get; set; }
+        public int MyProperty { get; set; }
+    }
+}
